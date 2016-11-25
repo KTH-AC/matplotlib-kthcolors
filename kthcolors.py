86a61d39da33ab5919e00d83583155c99c13287c
adaldo <antonio.adaldo.89@gmail.com>
2016-11-25 14:59:40 +0100

    added some colors

---
+++ kthcolors.py
@@ -0,0 +1,11 @@
+import matplotlib.colors as mplc
+
+kthcolors = dict(
+'kthblue': '#1954a6'
+'kthgreen': '#62922e'
+'kthred': '#9D102D'
+'kthyellow': '#fab919'
+'kthpink': '#d85497'
+)
+
+mplc.cnames.update(kthcolors)
