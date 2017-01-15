f57ec413556b04e7db4e98d228dc30fa81bbd817
adaldo <antonio.adaldo.89@gmail.com>
2017-01-15 22:46:04 +0100

    more colors and fixed install instructions

--- kthcolors/kthcolors.py
+++ kthcolors/kthcolors.py
@@ -2,10 +2,16 @@ import matplotlib.colors as mplc
 
 kthcolors = {
 'kthblue': '#1954a6',
+'kthlightblue': '#24a0d8',
 'kthgreen': '#62922e',
+'kthlightgreen': '#b0c92b',
 'kthred': '#9D102D',
+'kthlightred': '#e4363e',
 'kthyellow': '#fab919',
-'kthpink': '#d85497'
+'kthpink': '#d85497',
+'kthdarkgray': '#65656c',
+'kthmiddlegray': '#bdbcbc',
+'kthlightgray': '#e3e5e3'
 }
 
 mplc.cnames.update(kthcolors)
